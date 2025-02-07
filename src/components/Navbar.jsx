4e3a3d95b2e047bccf5e31de9931ee4004ca3871
Azim <azim210215@gmail.com>
2025-02-07 08:20:26 -0800

    Registration Logout Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import {AuthContext} from "../provider/AuthProvider"
 import { Link } from "react-router-dom"
 const Navbar = () => {
 
-  const {user} = useContext(AuthContext)
-  console.log(user)
+  const {user, logOut} = useContext(AuthContext)
+ 
   return (
     <div className='navbar bg-base-100 shadow-sm container px-4 mx-auto'>
       <div className='flex-1'>
@@ -62,7 +62,7 @@ const Navbar = () => {
               <div>Bid Requests</div>
             </li>
             <li className='mt-2'>
-              <button className='bg-gray-200 block text-center'>Logout</button>
+              <button onClick={logOut} className='bg-gray-200 block text-center'>Logout</button>
             </li>
           </ul>
         </div>
--- src/pages/Authentication/Registration.jsx
+++ src/pages/Authentication/Registration.jsx
@@ -1,11 +1,48 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import bgImage from "../../assets/images/register.jpg"
 import logo from "../../assets/images/logo.png"
 import { useContext } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
+import toast from "react-hot-toast";
 
 const Registration = () => {
-    const {createUser, signInWithGoogle} = useContext(AuthContext)
+  const navigate = useNavigate()
+    const {createUser, signInWithGoogle, updateUserProfile,setUser, user} = useContext(AuthContext)
+
+
+    const handleGoogleSignIn = async() =>{
+
+      try {
+        await signInWithGoogle()
+        toast.success('Signin Successful')
+      }
+      catch(err){
+        console.log(err)
+        toast.error(err?.message)
+      }
+  
+    }
+
+
+    const handleEmailSignIn = async(e) =>{
+      e.preventDefault();
+      const email = e.target.email.value;
+      const name = e.target.name.value;
+      const password = e.target.password.value;
+      const photoURL = e.target.photo.value;
+
+     try{
+     const result = await createUser(email,password)
+     await updateUserProfile(name, photoURL)
+     setUser({...user, photoURL:photoURL, displayName:name})
+     navigate("/")
+     toast.success('Signin Successful')
+     }
+     catch(err){
+      console.log(err)
+      toast.error(err?.message)
+    }
+    } 
   
     return (
       <div className='flex justify-center items-center min-h-[calc(100vh-306px)] my-4'>
@@ -23,7 +60,7 @@ const Registration = () => {
               Get Your Free Account Now.
             </p>
   
-            <div className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
+            <div onClick={handleGoogleSignIn} className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
               <div className='px-4 py-2'>
                 <svg className='w-6 h-6' viewBox='0 0 40 40'>
                   <path
@@ -59,7 +96,7 @@ const Registration = () => {
   
               <span className='w-1/5 border-b dark:border-gray-400 lg:w-1/4'></span>
             </div>
-            <form>
+            <form onSubmit={handleEmailSignIn}>
               <div className='mt-4'>
                 <label
                   className='block mb-2 text-sm font-medium text-gray-600 '
