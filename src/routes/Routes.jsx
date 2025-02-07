7e60f7634c0df00d10dbe3f049456a28d749bf6f
Azim <azim210215@gmail.com>
2025-02-07 04:04:19 -0800

    Login Function Added

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home";
+import Registration from "../pages/Authentication/Registration";
+import Login from "../pages/Authentication/Login";
+
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element:<Main></Main>,
+        children:[
+
+            {
+                index:true,
+                element:<Home></Home>
+            },
+            {
+                path: "/login",
+                element:<Login></Login>
+            },
+            {
+                path: "/registration",
+                element:<Registration></Registration>
+            }, {
+                path: "/",
+                element:<Home></Home>
+            }
+        ]
+    },
+
+])
+
+
+export default routes;
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// // tailwind.config.js
+// export default {
+//     content: [
+//       "./index.html",
+//       "./src/**/*.{js,ts,jsx,tsx}", // include all relevant files for Tailwind to scan
+//     ],
+//     theme: {
+//       extend: {
+//         fontFamily: {
+//           lato: ['Lato', 'serif'],
+//         },
+//       },
+//     },
+//     plugins: [],
+//   }
+  
